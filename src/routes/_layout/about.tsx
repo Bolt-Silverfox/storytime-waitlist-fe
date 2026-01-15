747e458e307a09947fd25e8760a5cebde5070def
Michelle Ndiangui <ndianguimichelle@gmail.com>
2026-01-15 20:44:08 +0300

    refact: about page design

--- src/routes/_layout/about.tsx
+++ src/routes/_layout/about.tsx
@@ -1,15 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 import AboutHero from "../../components/about/AboutHero";
 import CTASection from "../../components/CTASection";
 import AboutDetails from "../../components/about/AboutDetails";
 import Mission from "../../components/about/Mission";
 import Intentions from "../../components/about/Intentions";
+import DownloadModal from "../../components/DownloadModal";
 
 export const Route = createFileRoute("/_layout/about")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [isDownloadModalOpen, setIsDownloadModalOpen] = useState(false);
+
+  const openDownloadModal = () => {
+    setIsDownloadModalOpen(true);
+  };
+
   return (
     <>
       <AboutHero />
@@ -17,10 +25,13 @@ function RouteComponent() {
       <div className="mx-auto max-w-7xl px-4">
         <AboutDetails />
         <Mission />
-        <Intentions />
+        <Intentions openDownloadModal={openDownloadModal} />
       </div>
 
       <CTASection />
+      {isDownloadModalOpen && (
+        <DownloadModal onClose={() => setIsDownloadModalOpen(false)} />
+      )}
     </>
   );
 }
